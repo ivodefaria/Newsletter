ed6c34a9a3b21e4edc0c0ce9e0ac36daa62cdca3
Ivo Faria <ivofaria@MacBook-Pro-de-Ivo.local>
2022-04-07 16:13:16 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extende: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/2d10a9077a";
+
+  const options = {
+    method: "POST",
+    auth: "ivofaria:e5abe6fd09f82579aad908118b2f8991-us14"
+  };
+
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+
+      if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+      }else{
+        res.sendFile(__dirname + "/failure.html");
+      }
+
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is runing on port 3000.");
+});
